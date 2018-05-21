1c030b1dcc7d05f23da83ecd56240d21e994a17b
Mark Bina <Mark@BinaFamily.net>
2018-05-21 16:43:36 -0500

    moved folder

---
+++ PIC-USB-4550-BLINKING_LED/PIC_USB_4550_BLINKING_LED.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=PIC_USB_4550_BLINKING_LED.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/PIC_USB_4550_BLINKING_LED.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=picusb4550blinkingled.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/picusb4550blinkingled.x.tar
